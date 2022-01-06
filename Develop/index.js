03fc7dd8db19978b845a1ccad5d5d29f2338a6c0
sakme <alan@akme.us>
2022-01-06 08:41:25 -0500

    misc

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generatePage = require('.README.MD');
+const { writeFile, copyFile } = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
